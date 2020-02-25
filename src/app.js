1e632ad5ce54572dc8d937d946854b5fb6a24efc
Jean Dias <jeanmfdias@gmail.com>
2020-02-25 15:35:35 -0300

    validations to enter just 0 and 1 digits

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const $ = document.querySelector.bind(document);
 
-const binaryField = $('#binaryField');
+let binaryField = $('#binaryField');
+let errorsLabel = $('#errorsLabel');
+let decimalCovertedLabel = $('#decimalCovertedLabel');
 
 binaryField.addEventListener('keyup', (event) => {
+    let errors = '';
+    let value = binaryField.value
+        .split("")
+        .map((item) => { 
+            if (item == '0' || item == '1') {
+                return item
+            } else {
+                errors = `Don't enter with number different of 0 or 1.`
+            }
+        })
+        .join("");
     
+    errorsLabel.textContent = errors;
+    binaryField.value = value;
 });
